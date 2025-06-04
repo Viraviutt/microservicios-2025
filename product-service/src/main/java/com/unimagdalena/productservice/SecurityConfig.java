e784ff233a5111937f4e0ef8562fb61c6ec201fe
Carlos Lizarazo <carlosandr3zlizarazoromero@gmail.com>
2025-06-04 14:07:35 -0500

    Ajustes de prueba para microservicios product e inventory

--- product-service/src/main/java/com/unimagdalena/productservice/SecurityConfig.java
+++ product-service/src/main/java/com/unimagdalena/productservice/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+/*@Configuration
 public class SecurityConfig {
 
     @Bean
@@ -19,6 +19,21 @@ public class SecurityConfig {
                 .jwt()
             );
 
+        return http.build();
+    }
+}
+*/
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) //
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
         return http.build();
     }
 }
