907b3f3310e7d2a9ff30ace195835dfc67f07335
Carlos Lizarazo <161541967+CALR0@users.noreply.github.com>
2025-06-03 23:55:11 -0500

    Delete microservicios-2025 directory

--- microservicios-2025/order-service/src/main/java/com/unimagdalena/orderservice/respository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unimagdalena.orderservice.respository;
-
-import com.unimagdalena.orderservice.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, String> {
-}
--- microservicios-2025/payment-service/src/main/java/com/unimagdalena/paymentservice/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unimagdalena.paymentservice.repository;
-
-import com.unimagdalena.paymentservice.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, String> {
-}
